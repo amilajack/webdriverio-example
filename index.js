ffbde9a6b02d2df7ec59a05a2e26cf5c224570e0
Amila Welihinda <amilajack@gmail.com>
2018-06-20 15:28:55 -0700

    Misc commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const webdriverio = require('webdriverio')
+const assert = require('assert')
 
 const options = {
   desiredCapabilities: {
@@ -6,36 +7,17 @@ const options = {
   },
   // baseUrl: 'http://192.168.99.100:4444',
   host: '192.168.99.100',
-    port: 4444,
+  port: 4444,
 }
 
 const client = webdriverio.remote(options)
 
 client
   .init()
-  .url('https://google.com')
-  .setValue('.gsfi', 'foobar')
-  .click('input[type="submit"]')
-  .click('h3.r')
+  .url('https://example.com')
+  // .waitForExist('h1')
+  // .getText('h1')
   .getTitle()
   .end()
-  .then(console.log)
+  .then(result => assert.strictEqual(result.value, 'Example Domain'))
   .catch(console.log)
-
-// const {Builder, By, Key, until} = require('selenium-webdriver');
-
-// async function example() {
-//   let driver = await new Builder().forBrowser('firefox').build();
-//   try {
-//     await driver.get('http://www.google.com/ncr');
-//     await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-//     await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-//   }
-//   catch (e) {
-//     console.log(e)
-//   } finally {
-//     await driver.quit();
-//   }
-// };
-
-// example().then(console.log)
